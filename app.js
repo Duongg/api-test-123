551e802609c85b47bae436de271fea2a747162cd
Đinh Đức Dương <33210825+Duongg@users.noreply.github.com>
2020-09-16 11:22:56 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+app.post('/', function (req, res) {
+    const address = req.body;
+    console.log(address);
+    res.send(address);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
